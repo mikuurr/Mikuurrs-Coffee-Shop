c1d302e175dfed8b1c563c54714f326dc250c699
mikuurr <mikuurr@gmail.com>
2024-07-23 17:06:23 -0700

    added tab title

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -3,6 +3,11 @@ import BeansList from '../components/BeansList';
 import './Home.css';
 
 const Home = () => {
+
+  useEffect(() => {
+    document.title = "Mikuurrs Coffee Shop";
+  }, []);
+
   return (
     <div className="background">
       <BeansList />
