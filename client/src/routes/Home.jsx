d15b9f770275a5f2a61f442d52b87ea62665388e
mikuurr <mikuurr@gmail.com>
2024-07-23 18:17:51 -0700

    removed useeffect from home.jsx

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -3,11 +3,6 @@ import BeansList from '../components/BeansList';
 import './Home.css';
 
 const Home = () => {
-
-  useEffect(() => {
-    document.title = "Mikuurrs Coffee Shop";
-  }, []);
-
   return (
     <div className="background">
       <BeansList />
