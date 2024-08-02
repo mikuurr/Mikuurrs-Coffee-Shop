6a13d961f51626aa62320b8ff7fc0f7cdd538a35
mikuurr <mikuurr@gmail.com>
2024-08-02 16:46:13 -0700

    updated base url /api

--- client/src/apis/BeansFinder.js
+++ client/src/apis/BeansFinder.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import MockAdapter from 'axios-mock-adapter';
 
 const apiInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "https://mikuurrscoffeeshop.onrender.com/"
+  baseURL: process.env.REACT_APP_API_BASE_URL || "https://mikuurrscoffeeshop.onrender.com/api"
 });
 
 let mock = null;
