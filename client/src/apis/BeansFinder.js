fa2afca0296701f9c36f6db0efe1a3e2fe3bd004
mikuurr <mikuurr@gmail.com>
2024-08-06 18:07:09 -0700

    updated api call

--- client/src/apis/BeansFinder.js
+++ client/src/apis/BeansFinder.js
@@ -2,14 +2,14 @@ import axios from "axios";
 import MockAdapter from 'axios-mock-adapter';
 
 const apiInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "https://mikuurrscoffeeshop.onrender.com/api"
+  baseURL: process.env.REACT_APP_API_BASE_URL || "https://mikuurrscoffeeshop.onrender.com"
 });
 
 let mock = null;
 
 if (process.env.NODE_ENV === 'test') {
   mock = new MockAdapter(apiInstance);
-  mock.onGet('/').reply(200, { beans: [] });
+  mock.onGet('/beans').reply(200, { beans: [] });
 }
 
 const exportedObject = { apiInstance };
