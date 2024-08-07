9681850c3f953ad5aa9aec18161a4d6db8792b96
mikuurr <mikuurr@gmail.com>
2024-08-07 14:33:06 -0700

    defined api routes

--- server/server.js
+++ server/server.js
@@ -1,102 +1,101 @@
-require("dotenv").config();
-const express = require("express");
-const path = require("path");
-const cors = require("cors");
-const helmet = require("helmet");
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const helmet = require('helmet');
 const app = express();
-const db = require("./db");
+const db = require('./db');
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
+app.get('/api/beans', async (req, res) => {
+  try {
+    const results = await db.query('SELECT * FROM products');
+    res.status(200).json({
+      status: 'success',
+      results: results.rows.length,
+      data: {
+        beans: results.rows,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+  }
+});
+
+app.get('/api/beans/:id', async (req, res) => {
+  console.log(req.params.id);
+  try {
+    const results = await db.query('SELECT * FROM products where id = $1', [req.params.id]);
+    if (results.rows.length > 0) {
+      res.status(200).json({
+        status: 'success',
+        data: {
+          product: results.rows[0],
+        },
+      });
+    } else {
+      res.status(404).json({ status: 'error', message: 'Bean not found' });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+  }
+});
+
+app.post('/api/beans', async (req, res) => {
+  try {
+    const results = await db.query('INSERT INTO products (name, price) values ($1, $2) returning *', [req.body.name, req.body.price]);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        product: results.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+  }
+});
+
+app.put('/api/beans/:id', async (req, res) => {
+  try {
+    const results = await db.query('UPDATE products SET name = $1, price = $2, on_sale = $3 where id = $4 returning *', [req.body.name, req.body.price, req.body.on_sale, req.params.id]);
+    if (results.rows.length > 0) {
+      res.status(200).json({
+        status: 'success',
+        data: {
+          product: results.rows[0],
+        },
+      });
+    } else {
+      res.status(404).json({ status: 'error', message: 'Bean not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+  }
+});
+
+app.delete('/api/beans/:id', async (req, res) => {
+  try {
+    await db.query('DELETE FROM products where id = $1', [req.params.id]);
+    res.status(204).json({
+      status: 'success',
+    });
+  } catch (err) {
+    res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+  }
+});
+
 app.use(express.static(path.join(__dirname, '../client/build')));
-
-app.get("/api/beans", async (req, res) => {
-    try {
-        const results = await db.query("SELECT * FROM products");
-        res.status(200).json({
-            status: "success",
-            results: results.rows.length,
-            data: {
-                beans: results.rows,
-            },
-        });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ status: "error", message: "Internal Server Error" });
-    }
-});
-
-app.get("/api/beans/:id", async (req, res) => {
-    console.log(req.params.id);
-    try {
-        const results = await db.query("SELECT * FROM products where id = $1", [req.params.id]);
-        if (results.rows.length > 0) {
-            res.status(200).json({
-                status: "success",
-                data: {
-                    product: results.rows[0],
-                },
-            });
-        } else {
-            res.status(404).json({ status: "error", message: "Bean not found" });
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ status: "error", message: "Internal Server Error" });
-    }
-});
-
-app.post("/api/beans", async (req, res) => {
-    try {
-        const results = await db.query("INSERT INTO products (name, price) values ($1, $2) returning *", [req.body.name, req.body.price]);
-        res.status(201).json({
-            status: "success",
-            data: {
-                product: results.rows[0],
-            },
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ status: "error", message: "Internal Server Error" });
-    }
-});
-
-app.put("/api/beans/:id", async (req, res) => {
-    try {
-        const results = await db.query("UPDATE products SET name = $1, price = $2, on_sale = $3 where id = $4 returning *", [req.body.name, req.body.price, req.body.on_sale, req.params.id]);
-        if (results.rows.length > 0) {
-            res.status(200).json({
-                status: "success",
-                data: {
-                    product: results.rows[0],
-                },
-            });
-        } else {
-            res.status(404).json({ status: "error", message: "Bean not found" });
-        }
-    } catch (err) {
-        res.status(500).json({ status: "error", message: "Internal Server Error" });
-    }
-});
-
-app.delete("/api/beans/:id", async (req, res) => {
-    try {
-        await db.query("DELETE FROM products where id = $1", [req.params.id]);
-        res.status(204).json({
-            status: "success",
-        });
-    } catch (err) {
-        res.status(500).json({ status: "error", message: "Internal Server Error" });
-    }
-});
-
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
