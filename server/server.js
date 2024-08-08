b699facae6594e8b5e9573ef7383b6a76f9e3890
mikuurr <mikuurr@gmail.com>
2024-08-07 17:11:35 -0700

    changed fallback port back to 5000

--- server/server.js
+++ server/server.js
@@ -96,7 +96,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
