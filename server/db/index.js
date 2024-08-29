bae9e12940c4aa17f5d856971d4b532b1408197c
mikuurr <mikuurr@gmail.com>
2024-08-28 18:00:38 -0700

    added console log

--- server/db/index.js
+++ server/db/index.js
@@ -2,6 +2,8 @@ const { Pool } = require("pg");
 
 require('dotenv').config();
 
+console.log('Database URL:', process.env.DATABASE_URL);
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
